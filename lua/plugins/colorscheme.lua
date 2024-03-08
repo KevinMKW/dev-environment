2108498500fd500160058a7ea01e48ad415c5c3f
KevinMKW <mkwkvn@gmail.com>
2024-03-08 15:39:03 +0300

    Fix the transparency problem

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,8 @@ return {
 	lazy = false,
 	priority = 999,
 	config = function()
-		vim.cmd('colorscheme nightfox')
-	end
-} 
+		vim.cmd("colorscheme nightfox")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
+}
